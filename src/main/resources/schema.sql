a8c71ac7bc82f804e0e3e441e3dc9380ec999f1c
yezanrafed <yezan.rafed@tagpay.fr>
2020-04-06 21:40:08 +0200

    Initial commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    role INT UNSIGNED,
+    password VARCHAR(255),
+    created_at DATETIME,
+    updated_at DATETIME
+);
+
+CREATE TABLE boards (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    owner_id INT UNSIGNED,
+    created_at DATETIME,
+    updated_at DATETIME
+    FOREIGN KEY (owner_id) REFERENCES users(id);
+)
